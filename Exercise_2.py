fab9f0e4a18f9b75ae19b7036c971f923c104a1f
Almee Christian <almee@Mac.fios-router.home>
2025-03-09 18:45:25 -0400

    Pre course - 1 Almee'

--- Exercise_2.py
+++ Exercise_2.py
@@ -6,11 +6,27 @@ class Node:
  
 class Stack:
     def __init__(self):
+        self.head = None
+        self.size = 0
         
     def push(self, data):
+        if self.head is None:
+            self.head = Node(data)
+        else:
+            new_node = Node(data)
+            new_node.next = self.head
+            self.head = new_node
+        self.size += 1
         
     def pop(self):
-        
+        if self.head is None:
+            return None
+        else:
+            popped = self.head.data
+            self.head = self.head.next
+            self.size -= 1
+            return popped
+
 a_stack = Stack()
 while True:
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,8 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -11,12 +13,22 @@ class SinglyLinkedList:
         Takes O(1) time.
         """
         self.head = None
+        self.size = 0
 
     def append(self, data):
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        if not self.head:
+            self.head = ListNode(data, None)
+            self.size += 1
+        else:
+            curr = self.head
+            while curr.next:
+                curr = curr.next
+            curr.next = ListNode(data, None)
+            self.size += 1
         
     def find(self, key):
         """
@@ -24,9 +36,24 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        curr = self.head
+        while curr and curr.data != key:
+            curr = curr.next
+        return curr
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        curr = self.head
+        prev = None
+        while curr and curr.data != key:
+            prev = curr
+            curr = curr.next
+        if prev is None:
+            self.head = curr.next
+            curr.next = None
+        else:
+            prev.next = curr.next
+            curr.next = None
